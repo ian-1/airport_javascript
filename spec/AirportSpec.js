4ed3957c37f96c63c9b0816e9baeb925e5fb1260
ian-1 <iandavidwalton@mail.com>
2021-05-05 18:10:49 +0100

    add capacity, changeCapacity, planes and land functions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -10,4 +10,28 @@ describe ('Airport', function() {
       expect(airport.capacity()).toEqual(airport.DEFAULT_CAPACITY)
     });
   });
+
+  describe ('#changeCapacity', function() {
+    it ('changes capacity', function() {
+      const newCapactity = 100;
+      airport.changeCapacity(newCapactity);
+      expect(airport.capacity()).toEqual(newCapactity);
+    });
+  });
+
+  describe ('#planes', function() {
+    it ('automatically exists as an empty array', function() {
+      expect(airport.planes()).toEqual([])
+    });
+  });
+
+  describe ('when not stormy', function() {
+    describe ('#land', function() {
+      it ('will land a plane', function() {
+        const plane = 'p';
+        airport.land(plane);
+        expect(airport.planes()[0]).toEqual(plane);
+      });
+    });
+  });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,9 +1,22 @@
 class Airport {
   constructor() {
     this.DEFAULT_CAPACITY = 22
+    this.planesList = []
   };
 
   capacity() {
     return this.DEFAULT_CAPACITY;
+  };
+
+  planes() {
+    return this.planesList;
+  };
+
+  changeCapacity(newCapactity) {
+    this.DEFAULT_CAPACITY = newCapactity
+  };
+
+  land(plane) {
+    this.planesList.push(plane)
   }
 }
